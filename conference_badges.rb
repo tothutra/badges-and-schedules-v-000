9abbf9c40279f94f74c1c9979c5a24b23b554e3a
Tra To <tothutra@gmail.com>
2018-08-12 03:58:22 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,5 +3,6 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 def batch_badge_creator(attendees)
-
+  badges = []
+  attendees.each {|attendee| badges << badge_maker(attendee)}
 end
