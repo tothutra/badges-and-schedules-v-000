303c94b4f42202b8d31e8012bb973d9429c4ba2d
Tra To <tothutra@gmail.com>
2018-08-12 04:01:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,5 +9,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-
+  room_assignments = []
+  attendees.each {|attendee, index| room_assignments << "Hello, #{attendee}! You'll be assigned to room #{index}"}
+  return room_assignments
 end
